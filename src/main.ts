ce733b8feee465c92663f88073a512fdda9d88e4
Esteban Arias <tapitarias@gmail.com>
2024-07-18 12:16:53 -0500

    Set PORT to 8080

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PREFIX = '/v1.0/api';
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
+  app.enableCors();
   app.setGlobalPrefix(PREFIX);
-  await app.listen(8000);
+  await app.listen(8080);
 }
 bootstrap();
