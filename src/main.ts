847e6a9621cdc152780036342eeec2c559e7a9ed
Esteban Arias <tapitarias@gmail.com>
2024-07-18 11:03:36 -0500

    change port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix(PREFIX);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
