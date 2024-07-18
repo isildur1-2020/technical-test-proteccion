5d34a64f8af2e09f5868e86a3052c3b2d699be3c
Esteban Arias <tapitarias@gmail.com>
2024-07-18 11:49:39 -0500

    Disable CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
+import { joiValidationSchema } from './config/joi.validation';
 
 @Module({
-  imports: [FibonacciModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: joiValidationSchema,
+    }),
+    FibonacciModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PREFIX = '/v1.0/api';
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
   app.setGlobalPrefix(PREFIX);
   await app.listen(8000);
 }
