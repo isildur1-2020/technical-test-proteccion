87953800464a50a8a8af89ad71b5e76ddc0f5513
Esteban Arias <tapitarias@gmail.com>
2024-07-18 11:41:02 -0500

    Remove env loader to deploy on railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { EnvConfiguration } from './config/app.config';
-import { joiValidationSchema } from './config/joi.validation';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [EnvConfiguration],
-      validationSchema: joiValidationSchema,
-    }),
-    FibonacciModule,
-    MongooseModule.forRoot(process.env.MONGODB_URI),
-  ],
+  imports: [FibonacciModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
