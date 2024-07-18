39698ec92e6384d8f5b093770a116ffa3a4c395b
Esteban Arias <tapitarias@gmail.com>
2024-07-18 11:36:51 -0500

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PREFIX = '/v1.0/api';
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   app.setGlobalPrefix(PREFIX);
   await app.listen(8080);
 }
